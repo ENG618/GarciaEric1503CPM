65d321ea766e43e78351bbd3a1f5bf6024638279
eng618 <eng618@fullsail.edu>
2015-03-05 01:27:58 -0500

    Fixed error preventing settings fragment from displaying.  Added settings activity to manifest.  Successfully handles logging out and back in.

--- MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/main/MemoriesActivity.java
+++ MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/main/MemoriesActivity.java
@@ -1,11 +1,14 @@
 package com.garciaericn.memoryvault.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.garciaericn.memoryvault.R;
+import com.garciaericn.memoryvault.settings.SettingsActivity;
 
 /**
  * Full Sail University
@@ -29,5 +32,24 @@ public class MemoriesActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings: {
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            }
+            case R.id.action_add: {
+                // TODO: Launch add memory activity
+                return true;
+            }
+            case R.id.action_sync: {
+                // Handle in fragment
+                return false;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/settings/SettingsActivity.java
+++ MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/settings/SettingsActivity.java
@@ -1,7 +1,6 @@
 package com.garciaericn.memoryvault.settings;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
 
 import com.garciaericn.memoryvault.R;
@@ -14,8 +13,8 @@ import com.garciaericn.memoryvault.R;
 public class SettingsActivity extends ActionBarActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
         getFragmentManager().beginTransaction()
