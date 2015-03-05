8491f6b65c170676f92762354e70796a8ad89ec2
eng618 <eng618@fullsail.edu>
2015-03-05 00:52:55 -0500

    Set code for memories activity and fragment to load content.

--- MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/main/MemoriesActivity.java
+++ MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/main/MemoriesActivity.java
@@ -1,6 +1,11 @@
 package com.garciaericn.memoryvault.main;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.garciaericn.memoryvault.R;
 
 /**
  * Full Sail University
@@ -9,4 +14,18 @@ import android.support.v7.app.ActionBarActivity;
  */
 public class MemoriesActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_memories);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.memories_container, MemoriesFragment.newInstance())
+                .commit();
+    }
 }
---
+++ MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/main/MemoriesFragment.java
@@ -0,0 +1,40 @@
+package com.garciaericn.memoryvault.main;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.garciaericn.memoryvault.R;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 3/5/15.
+ */
+public class MemoriesFragment extends Fragment {
+
+    public MemoriesFragment() {
+        // Mandatory empty constructor
+    }
+
+    public static MemoriesFragment newInstance() {
+        return new MemoriesFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_memories, container, false);
+
+        // TODO: Set up view here...
+
+        return view;
+    }
+}
