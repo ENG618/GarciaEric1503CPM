84ea4a2065bc1e6e9329acc230a2b8269aefa79c
eng618 <eng618@fullsail.edu>
2015-03-05 01:34:16 -0500

    Loaded app_bar into settings activity, set it as up enables, and handled click to close settings.

--- MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/settings/SettingsActivity.java
+++ MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/settings/SettingsActivity.java
@@ -2,6 +2,8 @@ package com.garciaericn.memoryvault.settings;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.garciaericn.memoryvault.R;
 
@@ -12,13 +14,27 @@ import com.garciaericn.memoryvault.R;
  */
 public class SettingsActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         getFragmentManager().beginTransaction()
                 .replace(R.id.settings_container, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
