0737074fbc70e27ef9dff38ff7f1cf0ccc9006e3
eng618 <eng618@fullsail.edu>
2015-03-06 01:03:12 -0500

    Added new activity to manifest.  completed set up for navigating back to list with up button.

--- MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/addMemory/AddMemoryActivity.java
+++ MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/addMemory/AddMemoryActivity.java
@@ -1,7 +1,11 @@
 package com.garciaericn.memoryvault.addMemory;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.garciaericn.memoryvault.R;
 
@@ -12,9 +16,36 @@ import com.garciaericn.memoryvault.R;
  */
 public class AddMemoryActivity extends ActionBarActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_memory);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                Intent intent = new Intent();
+                setResult(RESULT_CANCELED, intent);
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/main/MemoriesActivity.java
+++ MemoryVault_Android/app/src/main/java/com/garciaericn/memoryvault/main/MemoriesActivity.java
@@ -6,8 +6,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.garciaericn.memoryvault.R;
+import com.garciaericn.memoryvault.addMemory.AddMemoryActivity;
 import com.garciaericn.memoryvault.settings.SettingsActivity;
 
 /**
@@ -17,6 +19,7 @@ import com.garciaericn.memoryvault.settings.SettingsActivity;
  */
 public class MemoriesActivity extends ActionBarActivity {
 
+    private static final int ADD_MEMORY_RC = 100;
     private Toolbar toolbar;
 
     @Override
@@ -42,7 +45,8 @@ public class MemoriesActivity extends ActionBarActivity {
                 return true;
             }
             case R.id.action_add: {
-                // TODO: Launch add memory activity
+                Intent addIntent = new Intent(this, AddMemoryActivity.class);
+                startActivityForResult(addIntent, ADD_MEMORY_RC);
                 return true;
             }
             case R.id.action_sync: {
@@ -51,5 +55,22 @@ public class MemoriesActivity extends ActionBarActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ADD_MEMORY_RC) {
+            if (resultCode == RESULT_OK) {
+                // TODO: Get extras and save new memory
+                showToast("Save successful");
+            } else if (resultCode == RESULT_CANCELED) {
+                showToast("No memory added");
+            }
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
